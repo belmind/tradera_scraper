8e1bbf2f8fe1099c1246a311854ecdb63a9aada2
Belmin Dervisevic <belmin.dervisevic@hotmail.com>
2018-06-29 15:32:20 +0200

    added date and ability to chose reseller
    spaghetti code, but added the ability to sort articles by reseller (Private, Company or both). Also added the date.

--- script.py
+++ script.py
@@ -1,25 +1,17 @@
 # -*- coding: utf-8 -*-
 import urllib2
+import datetime
 from bs4 import BeautifulSoup
 
 # clear the terminal
-for i in range(0,40):
-    print '\n'
-
+def clean():
+    for i in range(0,40):
+        print '\n'
+clean()
 # Welcome Message
-print('Python Scraper. Returnerar en lista på de senaste auktionerna från http://tradera.se')
+print('Python Scraper. Returnerar en lista på de senaste auktionerna från http://tradera.com')
 #specify the search query
-
 def main():
-        query = raw_input('Sökord:  ')
-        query = query.replace(' ', '%20') # Removes blankspaces and replaces them with %20
-
-        # specify the url
-        quote_page = 'https://www.tradera.com/search?q=' + query
-
-        # query the website and return the html to the variable ‘page’
-        page = urllib2.urlopen(quote_page)
-
         ##############
         items = {}
         prices = {}
@@ -27,6 +19,29 @@ def main():
         bids = {}
         ind = 0
         ##############
+
+        searchQuery = raw_input('Sökord:  ')
+        searchQuery = searchQuery.replace(' ', '%20') # Removes blankspaces and replaces them with %20
+        seller = ''
+        # Specify the url
+        # We want to filter between private or company resellers
+        filter = raw_input('Filtrera efter typ av säljare: Båda (0), Privat (1) eller Företag (2). ')
+
+        if filter == '0':
+            quote_page = 'https://www.tradera.com/search?q='
+            seller = 'Privat och Företag'
+        elif filter == '1':
+            quote_page = 'https://www.tradera.com/search?sellerType=Private&q='
+            seller = 'Enbart Privat'
+        elif filter == '2':
+            quote_page = 'https://www.tradera.com/search?sellerType=Company&q='
+            seller = 'Enbart Företag'
+
+        clean()
+        now = datetime.datetime.now()
+        quote_page = quote_page + searchQuery
+        # query the website and return the html to the variable ‘page’
+        page = urllib2.urlopen(quote_page)
         # parse the html using beautiful soup and store in variable `soup`
         soup = BeautifulSoup(page, 'html.parser')
 
@@ -36,7 +51,9 @@ def main():
         time_box = soup.find('span', attrs={'class': 'item-card-details-time-left'})
         bid_box = card_box = soup.find('span', attrs={'class': 'item-card-details-bids'})
 
-        #Print Column Names
+        # A lot of printing
+        print('Sökord: ' + searchQuery + '. Typ av säljare: ' + seller )
+        print('')
         print '%0s  %14s %18s  %12s  %24s' % ('#', 'Tid Kvar', 'Antal Bud', 'Pris', 'Artikelnamn')
         print('------------------------------------------------------------------------------------------------------------------------------------')
         # Get all the items
@@ -62,6 +79,7 @@ def main():
             times[ind] = card_box.text.strip()
             ind += 1
 
+
         #Output everything in a table
         for i in range(0,15):
             try:
@@ -71,6 +89,7 @@ def main():
                 pass
         # Funkar 100 % men då visas ej de andra artiklarna ex "21 Jul 12:22"
         print('------------------------------------------------------------------------------------------------------------------------------------')
+        print 'Sökning utfördes:' + now.strftime(" %H:%M %d-%m-%Y")
         pass
 
 while(True):
